b5fbadcf14bdd2f51f25b535ccda5285057a7b75
sergi <sergisiba@gmail.com>
2018-12-03 17:25:30 +0100

    First Commit. Started project

---
+++ src/BinarySearchTree/LinkedBinarySearchTree.java
@@ -0,0 +1,11 @@
+package BinarySearchTree;
+
+/**
+ * Makes a "unstable" binary search tree.
+ * @param <K> The key value, which will be compared and the natural way of order things.
+ * @param <V> The value stored in the binary Tree. Both seen as a SQL parameters k would be the primary key and v would
+ *           be the values associated with that key.
+ */
+public class LinkedBinarySearchTree<K, V> {
+
+}
