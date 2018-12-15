f7cf53984acbe81987584cff6cc4b9dd1b15c71b
sergi <sergisiba@gmail.com>
2018-12-16 00:11:55 +0100

    Testos de LBSTT fets. TODO: Pair

---
+++ Tests/BinarySearchTree/PairTest.java
@@ -0,0 +1,29 @@
+package BinarySearchTree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PairTest {
+
+    @Test
+    public void firstSecond() {
+        Pair<Integer, String> pair1 = new Pair<>(5, "abc");
+        Pair<Integer, String> pair2 = new Pair<>(3, "cba");
+        assertEquals(Integer.valueOf(5), pair1.first());
+        assertEquals(Integer.valueOf(3), pair2.first());
+        assertEquals("abc", pair1.second());
+        assertEquals("cba", pair2.second());
+    }
+
+    @Test
+    public void same() {
+        Pair<Integer, String> pair1 = new Pair<>(5, "abc");
+        Pair<Integer, String> pair2 = new Pair<>(5, "abc");
+        assertEquals(pair1, pair2);
+        pair2 = new Pair<>(3, "cba");
+        assertNotEquals(pair1, pair2);
+
+    }
+
+}
